d75655ce098add7934bfc3ee641d657c10fa5857
Yena Lee <lee00286@naver.com>
2021-10-07 22:19:08 +0900

    HappeningNow style  업데이트

--- client/src/components/HappeningNow/HappeningNow.js
+++ client/src/components/HappeningNow/HappeningNow.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import "./HappeningNow.css";
 import { Button, Typography } from 'antd';
+// Twitter Logo
+import Twitter from "../TwitterLogo";
 
 const { Title } = Typography;
 
 function HappeningNow() {
     return (
         <div className="happening-now-flexbox">
-            <div className="happening-now-left" />
+            {/* Left */}
+            <div className="happening-now-left">
+                <Twitter />
+            </div>
+            {/* Right */}
             <div className="happening-now-right">
-                <Title className="happening-now-text" level={1}>Happening now</Title>
-                <Title className="happening-now-text" level={2}>Join Twitter today.</Title>
+                <div className="happening-now-icon">
+                    <Twitter />
+                </div>
+                <div className="happening-now-text">
+                    <Title className="happening-now-text-one" level={1}>Happening now</Title>
+                    <Title className="happening-now-text-two" level={2}>Join Twitter today.</Title>
+                </div>
                 <div className="happening-now-buttons">
                     <Button className="happening-now-google">Sign up with Google</Button>
                     <Button className="happening-now-apple">Sign up with Apple</Button>
                     <Button className="happening-now-signup">Sign up with phone or email</Button>
                 </div>
                 <div className="happening-now-text-small">
-                    <p>By signing up, you agree to the Terms of Service and Privacy Policy,</p>
-                    <p>including Cookie Use.</p>
+                    <p>By signing up, you agree to the <a href="https://twitter.com/ko/tos">Terms of Service</a> and <a href="https://twitter.com/ko/privacy">Privacy</a></p>
+                    <p><a href="https://twitter.com/ko/privacy">Policy</a>, including <a href="https://help.twitter.com/ko/rules-and-policies/twitter-cookies">Cookie Use</a>.</p>
                 </div>
                 <div className="happening-now-signin">
-                    Already have an account? Sign in
+                    Already have an account? <a>Sign in</a>
                 </div>
             </div>
         </div>
